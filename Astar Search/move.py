b90bfb2145c0357db80290e2afb169850fbe7437
swapnilmankame1995 <swapnilum95@gmail.com>
2017-11-26 20:16:33 +0530

    updated on sunday night

--- Astar Search/move.py
+++ Astar Search/move.py
@@ -1,6 +1,7 @@
 import numpy as np
 import time
 count = 0
+count2 = 0
 
 r = np.load('Route_path_2.npy')
 # print r
@@ -14,8 +15,17 @@ for x,y in r:
     if (x == prev_x ):
         count = count + 1
     else:
-        print 'move ' + str(count) + ' steps and turn'
+        if (count != 0 ):
+            print 'move ' + str(count) + ' steps and turn'
+        if (y == prev_y ):
+            count2 = count2 + 1
+            print 'move ' + str(count2) + ' steps horizontally'
+        elif  (y != prev_y ):
+            print 'move ' + str(count2) + ' steps and turn'
         prev_x = x
+        prev_y = y
         count = 0
+        count2 = 0
+
 
 # print 'move ' + str(count) + ' steps and turn'
